6ea1daa15659031fba0c4d14eeab97c012f2cf93
Santiago Zaldivar <santiago.zaldivar.1997@gmail.com>
2020-07-26 23:00:00 -0500

    base of the landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,66 @@
 import React from "react";
-import logo from "./logo.svg";
-import { Button } from "antd";
+import { Layout, Menu, Typography } from "antd";
 import "./App.css";
+const { Header, Content, Footer } = Layout;
+const { Title } = Typography;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <Button type="primary">Button</Button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Layout
+      style={{
+        backgroundSize: "100% 100%"
+      }}
+    >
+      <Header
+        style={{
+          padding: "0",
+          position: "fixed",
+          zIndex: 1,
+          width: "100%",
+          backgroundColor: "transparent"
+        }}
+      >
+        <Menu
+          theme="light"
+          mode="horizontal"
+          style={{
+            background: "rgba(0,0,0,0.3)",
+            color: "#ffffff",
+            borderBottom: 0
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Menu.Item key="1">nav 1</Menu.Item>
+          <Menu.Item key="2">nav 2</Menu.Item>
+          <Menu.Item key="3">nav 3</Menu.Item>
+        </Menu>
+      </Header>
+      <Content
+        style={{
+          background:
+            "linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 1)), url('fighters.jpg') no-repeat center",
+          backgroundSize: "cover",
+          backgroundAttachment: "fixed",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
+        <Title
+          style={{
+            width: "500px",
+            height: "2000px",
+            textAlign: "center",
+            color: "white"
+          }}
+        >
+          Clones Esports
+        </Title>
+      </Content>
+      <Footer style={{ opacity: "0.2" }}>Footer</Footer>
+    </Layout>
   );
 }
 
